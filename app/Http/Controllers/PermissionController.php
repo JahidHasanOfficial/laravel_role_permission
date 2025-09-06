03f8489c50737135a6926c65d5193ecc72abd83b
jahid hasan <jahidhasanofficial23@gmail.com>
2025-09-06 13:42:36 +0600

    create edit delete

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -3,8 +3,69 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Support\Facades\Validator;
 
 class PermissionController extends Controller
 {
-    //
+    public function index() {
+        $permissions = Permission::all();
+        return view('permission.index', compact('permissions'));
+    }
+
+    public function create() {
+        return view('permission.create');
+    }
+
+
+
+
+      public function store(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|unique:permissions|min:3|max:255',
+        ]);
+
+        if ($validator->passes()) {
+           Permission::create([
+               'name' => $request->name
+           ]);
+
+           return redirect()->route('permissions.index')->with('success', 'Permission created successfully');
+        } else {
+            return redirect()->route('permissions.create')->withInput()->withErrors($validator);
+        }
+        
+    }
+
+        public function edit($id) {
+        $permission = Permission::findOrFail($id);
+
+        return view('permission.edit', compact('permission'));
+    }
+
+    public function update($id , Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|unique:permissions|min:3|max:255',
+        ]);
+
+        if ($validator->passes()) {
+            $permission = Permission::findOrFail($id);
+            $permission->name = $request->name;
+            $permission->save();
+            return redirect()->route('permissions.index')->with('success', 'Permission updated successfully');
+        } else {
+            return redirect()->route('permissions.edit', $id)->withInput()->withErrors($validator);
+        }
+        
+    }
+
+
+
+      public function destroy($id) {
+        $permission = Permission::findOrFail($id);
+        $permission->delete();
+        return redirect()->route('permissions.index')->with('success', 'Permission deleted successfully');
+        
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PermissionController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +16,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+        Route::get('/permission', [PermissionController::class, 'index'])->name('permissions.index');
+        Route::get('/permission/create', [PermissionController::class, 'create'])->name('permissions.create');
+        Route::post('/permission', [PermissionController::class, 'store'])->name('permissions.store');
+        Route::get('/permission/{id}/edit', [PermissionController::class, 'edit'])->name('permissions.edit');
+     Route::put('/permission/{id}', [PermissionController::class, 'update'])->name('permissions.update');
+
+
+        Route::delete('/permission/{id}', [PermissionController::class, 'destroy'])->name('permissions.destroy');
 });
 
 require __DIR__.'/auth.php';
