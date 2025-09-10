7660cd68d0a6ef73a3edb084ad20b435b0dc1601
jahid hasan <jahidhasanofficial23@gmail.com>
2025-09-10 17:44:26 +0600

    delete

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -125,6 +125,8 @@ class UserController extends Controller // implements HasMiddleware
      */
     public function destroy(string $id)
     {
-        //
+        $user = User::findOrFail($id);
+        $user->delete();
+        return redirect()->route('users.index')->with('success', 'User deleted successfully');
     }
 }
